76c651a829a21a9db44e65ba05e3c6dd732578a6
5jiji <60753482+5jiji@users.noreply.github.com>
2022-05-18 20:06:33 +0200

    Start of a warn command

---
+++ commands/warn.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { guildId } = require("../config.json")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName('warn')
+  .setDescription("warn a user")
+  .addUserOption(o => o
+    .setName("user")
+    .setDescription("the user to warn")
+    .setRequired(true))
+  .addStringOption(o => o
+    .setName("reason")
+    .setDescription("why this user should be warn?")),
+  async execute(interaction) {
+    interaction.reply({ content: "This command is not out yet!", ephemeral: true})
+  }
+}
