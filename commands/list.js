0dee87d01eec78f289ec971ff994de5d45d2224d
luihum <luistoo.favaro@outlook.com>
2022-05-18 15:04:59 -0300

    subcommand test but with actual code

--- commands/list.js
+++ commands/list.js
@@ -8,5 +8,11 @@ module.exports = {
       .addSubcommand(subcommand => subcommand
             .setName('warns')
             .setDescription('list warns')
-            )
+            ),
+    async execute(interaction) {
+		const subcommand = interaction.options.getSubcommand()
+		if (subcommand == "warns") { 
+			interaction.reply("cool it works")
+		}
+	}
 }
