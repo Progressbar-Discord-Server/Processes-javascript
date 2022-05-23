8a43211207740670eafdf1c6e843e6b3c72c225d
luihum <23081124+luihum@users.noreply.github.com>
2022-05-23 08:07:34 -0300

    fixed cache errors

--- commands/ban.js
+++ commands/ban.js
@@ -15,14 +15,14 @@ module.exports = {
       .setName("time")
       .setDescription("How long to ban this user?")),
   async execute(interaction) {
-    let member = interaction.options.getMember("user")
+    let member = await interaction.client.users.fetch(interaction.options.getMember("user"))
     let reason = interaction.options.getString("reason")
     const db = interaction.client.db.Bans
     if (!reason) reason = "No reason provided"
 
     if (interaction.options.getNumber("time") !== undefined) {let days = interaction.options.getNumber("time")}
     else {let days = null}
-
+    
     if (member.bannable) await member.ban({ days: days, reason: reason }).then(console.log).catch(error => {console.error(error);interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });return});
 
     else if (!user.bannable) {interaction.reply("I can't ban this member");return}
