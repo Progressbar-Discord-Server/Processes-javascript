96205845e3489bec91c4474106027737c62b383b
5jiji <60753482+5jiji@users.noreply.github.com>
2022-05-24 22:06:41 +0200

    Now a dev branch! with some change

--- commands/lock.js
+++ commands/lock.js
@@ -3,28 +3,33 @@ const { guildId } = require("../config.json")
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('lock')
-  .setDescription("lock a channel")
-  .addChannelOption(o => o
-    .setName("channel")
-    .setDescription("The channel to lock")
-    .setRequired(true))
-  .addStringOption(o => o
-    .setName("reason")
-    .setDescription("Why should this channel be locked?")),
+    .setName('lock')
+    .setDescription("lock a channel")
+    .addChannelOption(o => o
+      .setName("channel")
+      .setDescription("The channel to lock")
+      .setRequired(true))
+    .addStringOption(o => o
+      .setName("reason")
+      .setDescription("Why should this channel be locked?")),
   async execute(interaction) {
-    let channel = interaction.options.getChannel("channel"); 
+    let channel = interaction.options.getChannel("channel");
     let reason = interaction.options.getString("reason")
 
     if (!reason) reason = "No reason provided"
-    
-    channel.permissionOverwrites.edit(guildId, {
-      SEND_MESSAGES: false,
-      SEND_MESSAGES_IN_THREADS: false,
-      CREATE_PUBLIC_THREADS: false,
-      CREATE_PRIVATE_THREADS: false,
-    }, {reason: reason, type: 0})
 
-    interaction.reply("Channel locked")
-  } 
+    if (channel.type === "GUILD_NEWS") {
+      interaction.reply("No");
+      return
+    } else if (channel.type === "GUILD_TEXT") {
+      channel.permissionOverwrites.edit(guildId, {
+        SEND_MESSAGES: false,
+        SEND_MESSAGES_IN_THREADS: false,
+        CREATE_PUBLIC_THREADS: false,
+        CREATE_PRIVATE_THREADS: false,
+      }, { reason: reason, type: 0 })
+
+      interaction.reply("Channel locked")
+    }
+  }
 }
--- commands/unlock.js
+++ commands/unlock.js
@@ -3,17 +3,17 @@ const { guildId } = require("../config.json")
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('unlock')
-  .setDescription("unlock a channel")
-  .addChannelOption(o => o
-    .setName("channel")
-    .setDescription("the channel to lock")
-    .setRequired(true))
-  .addStringOption(o => o
-    .setName("reason")
-    .setDescription("Why should this channel be unlocked?")),
+    .setName('unlock')
+    .setDescription("unlock a channel")
+    .addChannelOption(o => o
+      .setName("channel")
+      .setDescription("the channel to lock")
+      .setRequired(true))
+    .addStringOption(o => o
+      .setName("reason")
+      .setDescription("Why should this channel be unlocked?")),
   async execute(interaction) {
-    let channel = interaction.options.getChannel("channel"); 
+    let channel = interaction.options.getChannel("channel");
     let reason = interaction.options.getString("reason")
 
     if (!reason) reason = "No reason provided"
@@ -27,8 +27,9 @@ module.exports = {
         SEND_MESSAGES_IN_THREADS: true,
         CREATE_PUBLIC_THREADS: true,
         CREATE_PRIVATE_THREADS: true,
-      }, {reason: reason, type: 0})
+      }, { reason: reason, type: 0 })
 
-      interaction.reply("Channel unlocked")}
-  } 
+      interaction.reply("Channel unlocked")
+    }
+  }
 }
