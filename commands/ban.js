784367a515f8f6a13de2cf96af768cafb76138bf
luihum <23081124+luihum@users.noreply.github.com>
2022-05-28 06:58:35 -0300

    fix ban command 2

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GuildMember } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
