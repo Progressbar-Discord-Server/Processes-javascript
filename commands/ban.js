b3a3daad057a198f11317a5390ad5b4cb77d6564
luihum <23081124+luihum@users.noreply.github.com>
2022-05-28 06:51:01 -0300

    rewrite ban command

--- commands/ban.js
+++ commands/ban.js
@@ -4,51 +4,41 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ban')
-    .setDescription("ban a user")
+    .setDescription("ban a member")
     .addUserOption(o => o
-      .setDescription("The user to ban")
-      .setName('user')
+      .setDescription("The member to ban")
+      .setName('member')
       .setRequired(true))
     .addStringOption(o => o
       .setName("reason")
-      .setDescription("Why should this user be banned?"))
+      .setDescription("Why should this member be banned?")
+      .setRequired(true))
     .addNumberOption(o => o
       .setName("time")
-      .setDescription("How long to ban this user?")),
+      .setDescription("How long to ban this member?")),
   async execute(interaction) {
-    let user = interaction.options.getMember("user")
-    let member = await interaction.client.users.fetch(interaction.options.getMember("user"))
-    let reason = interaction.options.getString("reason")
-    const db = interaction.client.db.Bans
-    let days = interaction.options.getNumber("time")
-    const replyEmbed = new MessageEmbed()
-    if (!reason) reason = "No reason provided"
-    if (!days) days = 0
-
-    await member.ban({ days: days, reason: reason })
-      .then(() => {
-        if (reason === "No reason provided") {
-          replyEmbed.setDescription(`${user.tag} has been banned`)
-          replyEmbed.setColor("#00FF00")
-        } else if (reason !== "No reaqon provided") {
-          replyEmbed.setDescription(`${user.tag} has been banned with the reason ${reason}`)
-          replyEmbed.setColor("#00FF00")
-        }
-      })
-      .catch(error => {
-        console.error(error)
-        replyEmbed.setDescription('There was an error while executing this command!')
-        replyEmbed.setColor("#FF0000")
-      })
-    if (replyEmbed.description === "There was an error while executing this command!") {
-      interaction.reply({ embeds: [replyEmbed], ephemeral: true })
-    } else if (replyEmbed.description === `${user.tag} has been banned` || replyEmbed.description === `${user.tag} has been banned with the reason ${reason}`) {
-      interaction.reply({ embeds: [replyEmbed] })
+    const member = interaction.options.getMember("member")
+    const guild = interaction.guild
+    const db = interaction.client.db.Cases
+    const reason = interaction.options.getString("reason") || "No reason provided"
+    const days = interaction.options.getNumber("time") || 0
+    if (!member.instanceof(GuildMember)) {
+      await guild.members.fetch(member)
     }
+    const dmEmbed = new MessageEmbed()
+      .setColor("#FF0000")
+      .setDescription(`You have been banned for: ${reason}`)
+    await member.user.send({ embeds: [dmEmbed] })
+    await member.ban({ days: days, reason: reason })
+    const replyEmbed = new MessageEmbed()
+      .setColor("#00FF00")
+      .setDescription(`**${member.user.tag} has been banned for:** ${reason}`)
+    await interaction.reply({ embeds: [replyEmbed] })
     db.create({
+      Executor: interaction.member.user.id,
+      userID: member.user.id,
       reason: reason,
-      Executor: interaction.member.user.tag,
-      userID: user.id
+      type: "ban"
     })
-  },
+  }
 };
