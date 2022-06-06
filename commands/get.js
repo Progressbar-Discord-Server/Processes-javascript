d248eb909dcbb12e21aa2af47d6d10a097ae7a3a
5jiji <60753482+5jiji@users.noreply.github.com>
2022-06-06 11:45:02 +0200

    Get command created

---
+++ commands/get.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('get')
+    .setDescription("Get something")
+    .addStringOption(o => o
+      .setName('what')
+      .setDescription("What do you want to get?")
+      .setRequired(true))
+      .addChoices({ name: 'Role Icon', value: 'ri' }, { name: 'Role Color', value: 'rc' }),
+  async execute(interaction) {
+    const choice = interaction.options.getString("what")
+    const guildRole = await interaction.guild.role.fetch()
+
+    if (choice = "ri") {
+      let array = []
+      guildRole.forEach(e => {
+        array.push(e.iconURL({format: 'png', size: 4096}))
+      });
+      interaction.reply(array)
+    }
+  }
+}
