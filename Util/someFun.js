0e53e69f258b14a1aeca5b82fb43ecedb3b27b5b
5jiji <60753482+5jiji@users.noreply.github.com>
2022-06-06 18:23:55 +0200

    vscode wanted to crash and it did

--- Util/someFun.js
+++ Util/someFun.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const Path = require('path')
 
 function CreateAndWrite(path, data) {
-  fs.writeFileSync(Path.join(__dirname, path), data)
+  fs.writeFileSync(Path.join(__dirname, '/..', path), data)
 }
 function ReadFile(path) {
-  fs.readFileSync(Path.join(__dirname, path))
+  fs.readFileSync(Path.join(__dirname, '/..', path))
 }
 
 module.exports = { CreateAndWrite, ReadFile }
