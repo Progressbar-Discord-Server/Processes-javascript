52e3cfb780604fb3aca8362945ebf148719f8622
5jiji <60753482+5jiji@users.noreply.github.com>
2022-06-06 21:55:04 +0200

    Found the bug... i think

--- Util/someFun.js
+++ Util/someFun.js
@@ -2,10 +2,15 @@ const fs = require('fs')
 const Path = require('path')
 
 function CreateAndWrite(path2, data) {
-  fs.writeFileSync(Path.join(__dirname, '/..', path2), data)
+  fs.writeFile(Path.join(__dirname, '/..', path2), data, err => {
+    if (err) throw err
+  })
 }
+
 function ReadFile(path2) {
-  fs.readFileSync(Path.join(__dirname, '/..', path2))
+  fs.readFile(Path.join(__dirname, '/..', path2), err => {
+    if (err) throw err
+  })
 }
 
 module.exports = { CreateAndWrite, ReadFile }
