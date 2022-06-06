dc5443277f64013798b26c1a4e0e5e99738b1b26
5jiji <60753482+5jiji@users.noreply.github.com>
2022-06-06 16:54:48 +0200

    ~~console.log Member *3~~

--- commands/get.js
+++ commands/get.js
@@ -17,26 +17,25 @@ module.exports = {
     if (choice === "ri") {
       let ArrayURL = []
       let nameRole = []
+
       guildRole.forEach(e => {
         if (e.iconURL()) {
           let icon = e.iconURL({ format: 'png', size: 4096 })
           let name = e.name
-          console.log(icon)
           ArrayURL.push(icon)
-          console.log(name)
           nameRole.push(name)
         }
       });
       let FormattedURLArray = []
-
+      
       for (i in ArrayURL) {
         let e = ArrayURL[i]
         let a = nameRole[i]
         FormattedURLArray.push(`${e} for ${a}`)
       }
-
       let FormattedURLList = FormattedURLArray.join('\n')
-      console.log(ArrayURL)
+
+      console.log(FormattedURLList)
       if (FormattedURLList && FormattedURLList.length > 2000) {
         interaction.followUp("I can't show you the result, too big of a message")
       }
