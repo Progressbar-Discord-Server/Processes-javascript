d50aa65ad310d5635ef51baf7d294fb6c59d7634
5jiji <60753482+5jiji@users.noreply.github.com>
2022-06-07 18:41:19 +0200

    Some quality of life feature

--- commands/get.js
+++ commands/get.js
@@ -6,21 +6,35 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('get')
     .setDescription("Get something")
-    .addStringOption(o => o
-      .setName('what')
-      .setDescription("What do you want to get?")
-      .addChoices({ name: 'Role Icon', value: 'ri' }, { name: 'Role Color', value: 'rc' })
-      .setRequired(true)),
+    .addSubcommand(sc => sc
+      .setName('Role Icon')
+      .setDescription("Getting All Icons of All Roles"))
+      .addStringOption(o => o
+        .setName("format")
+        .setDescription("In what format do you want the icons?")
+        .setRequired(true)
+        .addChoices({name: 'webp',value: 'webp'}, {name: 'png', value: 'png'}, {name: 'jpg', value: 'jpg'}, {name: 'jpeg', value: 'jpeg'}))
+        .addNumberOption(o => o
+          .setName('size')
+          .setDescription('At what size do you want the icon to be? (pixel)')
+          .setRequired(true))
+          .addChoices({name: '16',value: 16},{name: '32',value: 32},{name: '56',value: 56},{name: '64',value: 64},{name: '96',value: 96},{name: '128',value: 128},{name: '256',value: 256},{name: '300',value: 300},{name: '512',value: 512},{name: '600',value: 600},{name: '1024',value: 1024},{name: '2048',value: 2048},{name: '4096',value: 4096})
+    .addSubcommand(sc => sc
+      .setName('Role Color')
+      .setDescription("Getting All Colors of All Roles")),
   async execute(interaction) {
     await interaction.deferReply()
     const choice = interaction.options.getString("what")
     const guildRole = await interaction.guild.roles.fetch()
 
     if (choice === "ri") {
+      const format = interaction.options.getString("format")
+      const size = interaction.options.getNumber("size")
       let ArrayURL = []
+
       guildRole.forEach(e => {
         if (e.iconURL()) {
-          let icon = e.iconURL({ format: 'png', size: 4096 })
+          let icon = e.iconURL({ format: format, size: size })
           let name = e.name
           ArrayURL.push(`${icon} for ${name}\n`)
         }
@@ -29,7 +43,8 @@ module.exports = {
       if (ArrayURL) {
         console.log(ArrayURL)
         CreateAndWrite('/Tmp/log.txt', ArrayURL.toString())
-        interaction.followUp({files: [new MessageAttachment(ReadFile('/Tmp/log.txt'), 'result.txt')]})
+        const file = new MessageAttachment(ReadFile('Tmp/log.txt'), 'result.txt')
+        interaction.followUp({files: [file]})
       }
       else if (!ArrayURL) {
         interaction.followUp("There is no role with an icon")
@@ -47,7 +62,8 @@ module.exports = {
       if (ArrColor) {
         console.log(ArrColor)
         CreateAndWrite('/Tmp/log.txt', ArrColor.toString())
-        interaction.followUp({ files: [new MessageAttachment(ReadFile('/Tmp/log.txt'))]})
+        const file = new MessageAttachment(ReadFile('Tmp/log.txt'), 'result.txt')
+        interaction.followUp({ files: [file]})
       }
       else if (!ArrColor) {
         interaction.followUp("No role have color")
