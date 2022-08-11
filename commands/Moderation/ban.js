6384e4218963b05c7664d3a5bffdfc3c12f2abbd
5jiji <60753482+5jiji@users.noreply.github.com>
2022-08-11 16:11:27 +0200

    Fix of ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder, EmbedBuilder, GuildMember } = require('discord.js');
-const { logCha } = require('../../config.json')
+const { logCha } = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('ban')
-    .setDescription("ban a member")
-    .addUserOption(o => o
+    .setName("ban")
+    .setDescription("Ban a member")
+    .addUserOption(o =>o
       .setDescription("The member to ban")
-      .setName('member')
+      .setName("member")
       .setRequired(true))
     .addStringOption(o => o
       .setName("reason")
@@ -18,58 +18,63 @@ module.exports = {
       .setDescription("Is this command a joke command?")
       .setRequired(true))
     .addNumberOption(o => o
-      .setName("time")
-      .setDescription("How long to ban this member?")),
+      .setName("time").setDescription("How long to ban this member?")),
   async execute(interaction) {
-    interaction.deferReply()
+    await interaction.deferReply();
     const member = interaction.options.getMember("member", true);
     const guild = interaction.guild;
     const db = interaction.client.db.Cases;
     const reason = interaction.options.getString("reason", true);
     const days = interaction.options.getNumber("time") || 0;
     const joke = interaction.options.getBoolean("joke", true);
-
+    
     if (!(member instanceof GuildMember)) {
       await guild.members.fetch(member);
-    };
-
-    if (member.id === interaction.user.id) {
-      return interaction.followUp("Why do you want to ban yourself?");
     }
 
-    if (member.id === interaction.client.user.id) return interaction.followUp("❌ Why would you ban me? 😢");
+    const user = member.user;
 
     if (member.bannable) {
-    const dmEmbed = new EmbedBuilder()
-      .setColor("#f04a47")
-      .setDescription(`You have been banned for: ${reason}`);
-    const replyEmbed = new EmbedBuilder()
-      .setColor("#43b582")
-      .setDescription(`**${member.user.tag} has been banned for:** ${reason}`);
-    const logEmbed = new EmbedBuilder()
-      .setColor("#f04a47")
-      .addFields(
-        { name: '**User**', value: `${member}`, inline: true },
-        { name: '**Moderator**', value: `${interaction.member}`, inline: true },
-        { name: '**Reason**', value: `${reason}`, inline: true }
-      );
+      if (user.id === interaction.user.id) {
+        return interaction.followUp("Why do you want to ban yourself?");
+      }
+      if (user.id === interaction.client.user.id) return interaction.followUp("❌ Why would you ban me? 😢");
 
-      await member.user.send({ embeds: [dmEmbed] });
-      
+      const dmEmbed = new EmbedBuilder()
+        .setColor("#f04a47")
+        .setDescription(`You have been banned for: ${reason}`);
+      const replyEmbed = new EmbedBuilder()
+        .setColor("#43b582")
+        .setDescription(
+          `**${user.tag} has been banned for:** ${reason}`
+        );
+      const logEmbed = new EmbedBuilder()
+        .setColor("#f04a47")
+        .addFields(
+          { name: "**User**", value: `${member}`, inline: true },
+          {
+            name: "**Moderator**",
+            value: `${interaction.member}`,
+            inline: true,
+          },
+          { name: "**Reason**", value: `${reason}`, inline: true }
+        );
+
+      await user.send({ embeds: [dmEmbed] });
+
       if (!joke) {
         await member.ban({ days: days, reason: reason });
         db.create({
           Executor: interaction.user.id,
-          userID: member.user.id,
+          userID: user.id,
           reason: reason,
-          type: "ban"
+          type: "ban",
         });
-        let logChannel = await guild.channels.fetch(logCha)
-        logChannel.send({ embeds: [logEmbed] })
+        let logChannel = await guild.channels.fetch(logCha);
+        logChannel.send({ embeds: [logEmbed] });
       }
-  
+
       await interaction.followUp({ embeds: [replyEmbed] });
-    }
-    else return interaction.followUp('Erf, i can\'t do that')
-  }
-}
+    } else return interaction.followUp("Erf, i can't do that");
+  },
+};
