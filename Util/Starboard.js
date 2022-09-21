d86776e6552b05f3df7f5471c74b788460aa9604
5jiji <60753482+5jiji@users.noreply.github.com>
2022-09-21 19:59:30 +0200

    Starboard With Reply Messages!

--- Util/Starboard.js
+++ Util/Starboard.js
@@ -1,4 +1,5 @@
-const { ButtonBuilder, ActionRowBuilder, EmbedBuilder } = require("discord.js")
+const { Message } = require("discord.js")
+const { ButtonBuilder, ActionRowBuilder, EmbedBuilder, MessageType } = require("discord.js")
 const { starBoardEmoji } = require("../config.json")
 
 function StarboardAdd(reaction) {
@@ -19,18 +20,18 @@ async function brainAdd(reaction, setting) {
   const db = reaction.client.db.Star
 
   let ChaThd
-  if (setting.thread) ChaThd = await reaction.client.channels.cache.get(setting.ChaId).threads.fetch(setting.ThdId)
+  if (setting.thread && setting.ThdId) ChaThd = await reaction.client.channels.cache.get(setting.ChaId).threads.fetch(setting.ThdId)
   else ChaThd = reaction.client.channels.cache.get(setting.ChaId)
 
   if (reaction.users.cache.has(message.author.id)) rcount--
 
   const dbData = await db.findOne({ where: { messageId: message.id, emoji: setting.emoji } })
 
-  if (rcount >= 5 && !dbData) {
-    const starEmbed = createEmbed(message)
-    const button = createButton(message.url)
+  if (rcount >= 1 && !dbData) {
+    const starEmbed = await createEmbed(message)
+    const buttons = await createButton(message)
 
-    ChaThd.send({ content: `${setting.emoji} **${rcount}** | <#${message.channel.id}>`, embeds: [starEmbed], components: [button] })
+    ChaThd.send({ content: `${setting.emoji} **${rcount}** | <#${message.channel.id}>`, embeds: [starEmbed], components: buttons })
       .then(async ownmessage => {
         await db.create({
           messageId: message.id,
@@ -41,12 +42,12 @@ async function brainAdd(reaction, setting) {
       .catch(console.error)
   }
   else if (dbData) {
-    const starEmbed = createEmbed(message)
-    const button = createButton(message.url)
+    const starEmbed = await createEmbed(message)
+    const buttons = await createButton(message)
 
     ChaThd.messages.fetch(dbData.messageIdBot)
       .then((e) => {
-        e.edit({ content: `${setting.emoji} **${rcount}** | <#${message.channel.id}>`, embeds: [starEmbed], components: [button] })
+        e.edit({ content: `${setting.emoji} **${rcount}** | <#${message.channel.id}>`, embeds: [starEmbed], components: buttons })
       })
   }
 }
@@ -65,8 +66,8 @@ async function brainRemove(reaction, setting) {
   const dbData = await db.findOne({ where: { messageId: message.id, emoji: setting.emoji } })
   if (!dbData) return
 
-  const starEmbed = createEmbed(message)
-  const button = createButton(message.url)
+  const starEmbed = await createEmbed(message)
+  const button = await createButton(message)
 
   ChaThd.messages.fetch(dbData.messageIdBot)
     .then(e => {
@@ -74,35 +75,55 @@ async function brainRemove(reaction, setting) {
     })
 }
 
-function createEmbed(message) {
-  const avatar = message.author.avatarURL({ extension: "png", size: 4096 })
+async function createEmbed(MainMessage) {
+  const avatar = MainMessage.author.avatarURL({ extension: "png", size: 4096 })
 
   const embed = new EmbedBuilder()
-    .setAuthor({ name: message.author.tag, iconURL: typeof avatar === "string" ? avatar : undefined })
+    .setAuthor({ name: (MainMessage.author.tag.endsWith("#0000") ? MainMessage.author.tag.slice(0, -5) : MainMessage.author.tag), iconURL: typeof avatar === "string" ? avatar : undefined })
     .setColor(Math.floor(Math.random() * 16777215).toString(16))
     .setTimestamp(new Date());
 
-  let yesnt = true
-
-  if (message.content) embed.setDescription(message.content)
-  else {
-    message.embeds.forEach(e => {
-      if (e.description && yesnt) { embed.setDescription(e.description); yesnt = false }
-    })
+  if (MainMessage.content) embed.setDescription(MainMessage.content)
+  else if (MainMessage.embeds[0]) {
+    let yesnt = true
+    MainMessage.embeds.forEach(e => { if (e.description && yesnt) { embed.setDescription(e.description); yesnt = false } })
   }
 
-  if (message.attachments.size) embed.setImage(message.attachments.first().url)
+  if (MainMessage.attachments.size) embed.setImage(MainMessage.attachments.first().url)
+
+  if (MainMessage.type === MessageType.Reply) {
+    let reference = await MainMessage.fetchReference()
+    if (reference.content) embed.setFields({ name: "Replied to:", value: reference.content })
+    else if (reference.embeds[0]) {
+      let yesnt = true
+      reference.embeds.forEach(e => { if (e.description && yesnt) { embed.setFields({ name: "Replied to:", value: e.description }); yesnt = false } })
+    }
+  }
 
   return embed
 }
 
-function createButton(url) {
-  return new ActionRowBuilder()
+async function createButton(message) {
+  let mainButton = new ActionRowBuilder()
     .addComponents(new ButtonBuilder()
       .setLabel("Original Message")
       .setStyle("Link")
-      .setURL(url)
+      .setURL(message.url)
     )
+
+  if (message.type === MessageType.Reply) {
+    let reference = await message.fetchReference()
+    let replyButton = new ActionRowBuilder()
+      .addComponents(new ButtonBuilder()
+        .setLabel("Reply Message")
+        .setStyle("Link")
+        .setURL(reference.url)
+      )
+    return [mainButton, replyButton]
+  }
+  else {
+    return [mainButton]
+  }
 }
 
 module.exports = { StarboardAdd, StarboardRemove }
