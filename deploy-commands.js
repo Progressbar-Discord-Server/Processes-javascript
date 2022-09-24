ce5dac82d2b0e3ee1f9aa2a2cc4ffca968ec5ed9
5jiji <60753482+5jiji@users.noreply.github.com>
2022-09-24 18:30:25 +0200

    if rest.put gives a promise, a try block isn't needed

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,53 +3,50 @@ const { Routes } = require('discord-api-types/v10');
 const { token, clientId, beta } = require('./config.json');
 
 if (require.main === module) {
-	const fs = require('node:fs');
-	const commands = [];
+  const fs = require('node:fs');
+  const commands = [];
 
-	for (const folder of fs.readdirSync(`${__dirname}/commands`)) {
-		for (const file of fs.readdirSync(`${__dirname}/commands/${folder}`).filter(file => file.endsWith(".js"))) {
-			try {
-				const command = require(`${__dirname}/commands/${folder}/${file}`);
-				commands.push(command.data.toJSON());
-			} catch (err) {
-				console.error(err);
-			}
-		}
-	}
+  for (const folder of fs.readdirSync(`${__dirname}/commands`)) {
+    for (const file of fs.readdirSync(`${__dirname}/commands/${folder}`).filter(file => file.endsWith(".js"))) {
+      try {
+        const command = require(`${__dirname}/commands/${folder}/${file}`);
+        commands.push(command.data.toJSON());
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
-	const contextMenu = [];
+  const contextMenu = [];
 
-	for (const type of fs.readdirSync(`${__dirname}/context menu`)) {
-		for (const folder of fs.readdirSync(`${__dirname}/context menu/${type}`)) {
-			for (const file of fs.readdirSync(`${__dirname}/context menu/${type}/${folder}`).filter(file => file.endsWith(".js"))) {
-				try {
-					const contextmenu = require(`${__dirname}/context menu/${type}/${folder}/${file}`);
-					contextMenu.push(contextmenu.data.toJSON());
-				} catch (err) {
-					console.error(err);
-				}
-			}
-		}
-	}
+  for (const type of fs.readdirSync(`${__dirname}/context menu`)) {
+    for (const folder of fs.readdirSync(`${__dirname}/context menu/${type}`)) {
+      for (const file of fs.readdirSync(`${__dirname}/context menu/${type}/${folder}`).filter(file => file.endsWith(".js"))) {
+        try {
+          const contextmenu = require(`${__dirname}/context menu/${type}/${folder}/${file}`);
+          contextMenu.push(contextmenu.data.toJSON());
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  }
 
-	const all = [];
-	commands.forEach(e => { if (!beta && e.name !== "test" || beta) all.push(e) })
-	contextMenu.forEach(e => all.push(e));
-	send(all, token, clientId)
+  const all = [];
+  commands.forEach(e => { if (!beta && e.name !== "test" || beta) all.push(e) })
+  contextMenu.forEach(e => all.push(e));
+  send(all, token, clientId)
 }
 
 async function send(all, token, clientId) {
-	const rest = new REST({ version: '10' }).setToken(token);
-	try {
-		console.log('Started refreshing application (/) commands.');
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: all },
-		).catch(e => { console.error(e) });
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
+  const rest = new REST({ version: '10' }).setToken(token);
+
+  console.log(`Started refreshing ${all.length} slash commands.`);
+  await rest.put(
+    Routes.applicationCommands(clientId),
+    { body: all },
+  ).catch(e => { console.error(e) });
+  console.log(`Successfully reloaded ${all.length} slash commands.`);
 };
 
 
