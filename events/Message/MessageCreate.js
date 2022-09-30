1216597061f0358e3dbaeae02e4f51d3e0a5b11b
5jiji <60753482+5jiji@users.noreply.github.com>
2022-09-30 14:13:05 +0200

    Forgot half of the fix

--- events/Message/MessageCreate.js
+++ events/Message/MessageCreate.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "messageCreate",
   on: true,
   async execute(messages) {
-    if (messages.partial) {await message.fetch().catch(e => {console.error("A partial message couldn't get fetch.")})}
+    if (messages.partial) {await messages.fetch().catch(e => {console.error("A partial message couldn't get fetch.")})}
     if (messages.author.id === client.user.id) return;
     
     client.messages.forEach(e => { if (e.code) e.code(messages).catch(console.error) })
