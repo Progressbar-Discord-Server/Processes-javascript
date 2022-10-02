3b704d60b2ed95095e0e0de30fc50a65f17040a3
5jiji <60753482+5jiji@users.noreply.github.com>
2022-10-02 17:49:17 +0200

    Forgot the reddit config

--- messages/Misc/reddit.js
+++ messages/Misc/reddit.js
@@ -1,11 +1,11 @@
-const { redditCha } = require("../../config.json")
+const { redditCha, reddit } = require("../../config.json")
 
 module.exports = {
   message: "reddit",
   onlyCode: true,
   async code(messages) {
     await messages.fetch()
-    if (messages.channel.id === redditCha && messages.embeds[0]?.title) {
+    if (messages.channel.id === redditCha && messages.embeds[0]?.title && reddit) {
       this.execute(messages);
     }
   },
