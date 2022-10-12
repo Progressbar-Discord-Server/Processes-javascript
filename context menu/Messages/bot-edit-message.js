8b905c28c8bb5ac3001cedc480b20ef66b91e01f
5jiji <60753482+5jiji@users.noreply.github.com>
2022-10-12 17:22:48 +0200

    Fix of the context menu for editing bot messages

--- context menu/Messages/bot-edit-message.js
+++ context menu/Messages/bot-edit-message.js
@@ -1,14 +1,12 @@
-const { codeBlock } = require('discord.js');
-const { ContextMenuCommandInteraction } = require('discord.js');
-const { ContextMenuCommandBuilder, ModalBuilder, TextInputStyle, ActionRowBuilder, TextInputBuilder } = require('discord.js');
+const { ContextMenuCommandBuilder, ModalBuilder, TextInputStyle, ActionRowBuilder, TextInputBuilder, codeBlock } = require('discord.js');
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
     .setName('Edit Bot Message')
     .setType(3),
   async execute(interaction) {
-    if (interaction.targetMessage.author.id !== interaction.client.user.id) return;
-    await interaction.targetMessage.fetch()
+    if (interaction.targetMessage.author.id !== interaction.client.user.id) return interaction.reply({content: `This isn't a bot message, please chose a message from myself, <@${interaction.client.user.id}>`, ephemeral: true});
+    Promise.all([interaction.targetMessage.fetch(), interaction.targetMessage.channel.fetch()])
     await interaction.showModal(new ModalBuilder().setCustomId("edit-message").setTitle("Edit Message").addComponents(new ActionRowBuilder().addComponents(new TextInputBuilder()
     .setCustomId("message")
     .setLabel("message")
@@ -18,7 +16,7 @@ module.exports = {
     interaction.awaitModalSubmit({time: 1_000_000}).then(e => {
       interaction.targetMessage.edit(e.fields.getField("message").value)
       .then(() => e.reply({content: "Done", ephemeral: true}))
-      .catch(err => {e.reply({content: `An error occured: ${codeBlock(err)}`, ephemeral: true})})
+      .catch(err => {e.client.debugCha.send({content: `An error occured: ${codeBlock(err)} for editing a bot message`, ephemeral: true});e.reply({content: `An Error Occured: ${codeBlock(err)}`})})
     })
   }
 }
