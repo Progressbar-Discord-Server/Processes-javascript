99859232731fa8b4e775304c6825082dc12525b0
5jiji <60753482+5jiji@users.noreply.github.com>
2022-11-29 15:28:14 +0100

    forgot ephemeral

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -9,7 +9,7 @@ module.exports = {
       color: Math.floor(Math.random() * 16777215),
       description: `Pong!\nLatency of the bot:${Date.now() - interaction.createdTimestamp}ms`
     }
-    await interaction.reply({embeds: [replyEmbed]})
+    await interaction.reply({embeds: [replyEmbed], ephemeral: true})
     replyEmbed.description += `\nRound-trip: ${Date.now() - interaction.createdTimestamp}ms`
     return await interaction.editReply({embeds: [replyEmbed]})
   }
