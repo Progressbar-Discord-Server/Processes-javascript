bf852de6ca2f769f51ab62b3587b379405dfbc21
5jiji <60753482+5jiji@users.noreply.github.com>
2022-11-29 15:25:10 +0100

    ping.js

---
+++ commands/Misc/ping.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Pong!'),
+  async execute(interaction) {
+    const replyEmbed = {
+      color: Math.floor(Math.random() * 16777215),
+      description: `Pong!\nLatency of the bot:${Date.now() - interaction.createdTimestamp}ms`
+    }
+    await interaction.reply({embeds: [replyEmbed]})
+    replyEmbed.description += `\nRound-trip: ${Date.now() - interaction.createdTimestamp}ms`
+    return await interaction.editReply({embeds: [replyEmbed]})
+  }
+}
